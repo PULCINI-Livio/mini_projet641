2574caedcc39a05b9a0c9d317abedd738b997cfd
CypGttn <154144790+CypGttn@users.noreply.github.com>
2024-05-13 14:42:56 +0200

    getters et setters

--- Bavard.java
+++ Bavard.java
@@ -1,3 +1,13 @@
 public class Bavard implements PapotageListener{
     protected String nom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    
 }
