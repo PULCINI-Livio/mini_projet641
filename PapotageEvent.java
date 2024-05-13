a2a1b3f2d5e736b6666aacc351a2fe6066e85e46
PULCINI-Livio <pulcinilivio04@gmail.com>
2024-05-13 14:43:37 +0200

    Update PapotageEvent.java

--- PapotageEvent.java
+++ PapotageEvent.java
@@ -1,6 +1,16 @@
-public class PapotageEvent {
+import java.util.EventObject;
+
+public class PapotageEvent extends EventObject{
     protected String sujet;
     protected String corps;
+
+    public PapotageEvent(Object source, String unSujet, String unCorps) {
+        super(source);
+        this.sujet = unSujet;
+        this.corps = unCorps;
+    }
+
+    
 
     // Methodes
     public void addPapotageListener() {
