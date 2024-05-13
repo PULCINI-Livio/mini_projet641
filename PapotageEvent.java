5f45df26354d4f7eba405f75fa42a07391ac2ff5
PULCINI-Livio <pulcinilivio04@gmail.com>
2024-05-13 14:32:37 +0200

    creation papotageEvent et listener

---
+++ PapotageEvent.java
@@ -0,0 +1,13 @@
+public class PapotageEvent {
+    String sujet;
+    String corps;
+
+    // Methodes
+    public void addPapotageListener() {
+
+    }
+
+    public void removePapotageListener() {
+        
+    }
+}
